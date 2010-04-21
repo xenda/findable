cb23f84c7379f81d04e82ac3ce0652adcbb0e70b
Alvaro Pereyra Rabanal <alvaro.pereyra@srdperu.com>
2010-04-21 10:17:01 -0500

    Up

--- lib/app/helpers/findable_helper.rb
+++ lib/app/helpers/findable_helper.rb
@@ -1,11 +1,15 @@
 module FindableHelper
 
-  def ul_tag(collection,li_options = {},ul_options={})
+  def ul_tag(collection,li_options = {},ul_options={},not_found="No se encontraron")
     
-    list_items = collection.map{|item|
-      result = yield(item)
-      content_tag(:li,result,li_options)
-    }.join
+    if collection.nil? or collection.empty?
+        list_items = content_tag(:li,not_found)
+    else
+      list_items = collection.map{|item|
+        result = yield(item)
+        content_tag(:li,result,li_options)
+      }.join
+    end
     
     content_tag(:ul,list_items,ul_options)
     
--- lib/findable.rb
+++ lib/findable.rb
@@ -1,21 +1,32 @@
 # Findable
 module Findable
+  
+  def self.included(base)
 	
-   def self.included(base)
-      
-      #parent_table = base.table_name.gsub!("_details","")
-
-			base.class_eval do
-				
-        	named_scope :with_fields, lambda { |fields|  fields = "*" if fields.blank?; {:select=>[fields]}  }
-          named_scope :last, lambda{|limit| {:limit=>limit, :order=>"created_at DESC"}}
-	    end
-
-	  end
+	base.class_eval do
 	
+	
+    named_scope :with_fields, lambda { |fields|  fields = "*" if fields.blank?; {:select=>[fields]}  }
+    named_scope :limit, lambda{|limit| {:limit=>limit, :order=>"created_at DESC"}}
+    named_scope :random, lambda{|klass| puts self; {:select=>"*, id as r1", :joins=>"(SELECT ROUND(RAND()*( SELECT MAX (id) from #{klass.name} #{klass.class})) as id ) as r2", :conditions=>"r1.id >= r2.id", :order=>"r1.id", :limit => 1}  }
+    
+  end
+  
+  end  
+          # SELECT <COLUMN> FROM <TABLE> AS r1
+          # JOIN (SELECT ROUND(
+          #   RAND( ) * (
+          #     SELECT MAX( id ) FROM <TABLE>)
+          #   ) AS id
+          # ) AS r2
+          # WHERE r1.id >= r2.id
+          # ORDER BY r1.id ASC
+          # LIMIT 1;
+          
+          	
 end
 
-ActiveRecord::Base.send :include, Findable
+ActiveRecord::Base.send :include,Findable
 
 
 %w{ models controllers helpers }.each do |dir|
